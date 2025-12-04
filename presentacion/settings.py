3e62a589b8ea30a6e2a2cfa81fd5bcbcf292658b
jairosolorzano861@gamil.com <jairosolorzano861@gmail.com>
2025-12-03 22:53:04 -0500

    Config: agregar STATIC_ROOT para producción

--- presentacion/settings.py
+++ presentacion/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.onrender.com',
+    'localhost',]
 
 
 # Application definition
@@ -116,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
